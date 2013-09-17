32dbdfe86f42b79b32c068a74ae092d8a98206b9
solusipse <solus1ps3@gmail.com>
2013-09-18 01:02:00 +0200

    increased slug array size

--- fiche.c
+++ fiche.c
@@ -84,7 +84,7 @@ void *thread_connection(void *args)
 
     if (status != -1)
     {
-        char slug[SLUG_SIZE+1];
+        char slug[SLUG_SIZE+8];
         generate_url(buffer, slug, SLUG_SIZE+1);
         save_log(slug, data.ip_address, data.hostname);
         char response[strlen(slug) + strlen(DOMAIN) + 2];
