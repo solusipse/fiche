019129f941f22f050aca4fd9a2d398f6f69f33af
sugoiuguu <sugoiuguu@tfwno.gf>
2018-02-15 00:58:14 +0000

    Removed some unnecessary heap allocs

--- fiche.c
+++ fiche.c
@@ -718,10 +718,9 @@ static int create_directory(char *output_dir, char *slug) {
     size_t len = strlen(output_dir) + strlen(slug) + 2;
 
     // Generate a path
-    char *path = malloc(len);
-    if (!path) {
-        return -1;
-    }
+    char path[len];
+    memset(path, 0, sizeof(path));
+
     snprintf(path, len, "%s%s%s", output_dir, "/", slug);
 
     // Create output directory, just in case
@@ -732,8 +731,6 @@ static int create_directory(char *output_dir, char *slug) {
         path,
         S_IRWXU | S_IRGRP | S_IROTH | S_IXOTH | S_IXGRP
     );
-
-    free(path);
 
     return r;
 }
@@ -746,17 +743,14 @@ static int save_to_file(const Fiche_Settings *s, uint8_t *data, char *slug) {
     size_t len = strlen(s->output_dir_path) + strlen(slug) + strlen(file_name) + 3;
 
     // Generate a path
-    char *path = malloc(len);
-    if (!path) {
-        return -1;
-    }
+    char path[len];
+    memset(path, 0, sizeof(path));
 
     snprintf(path, len, "%s%s%s%s%s", s->output_dir_path, "/", slug, "/", file_name);
 
     // Attempt file saving
     FILE *f = fopen(path, "w");
     if (!f) {
-        free(path);
         return -1;
     }
 
@@ -765,12 +759,10 @@ static int save_to_file(const Fiche_Settings *s, uint8_t *data, char *slug) {
 
     if ( fprintf(f, "%s", data) < 0 ) {
         fclose(f);
-        free(path);
         return -1;
     }
 
     fclose(f);
-    free(path);
 
     return 0;
 }
